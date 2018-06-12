d1322c186d00550c0b8fa524a67b3a3ec291d200
Laura Darlak <laura@polyporedesign.com>
2018-06-12 13:09:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 // alter the original array that's passed in (mutate array's value)
  function destructivelyAddElementToBeginningOfArray(array, element){
    array.unshift(element)
-   return array;
+   return array
  }
  
  // return a new array and not modify the original
