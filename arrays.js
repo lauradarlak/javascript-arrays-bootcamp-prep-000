0ddd51da414ff39942061d977b388e043a30e2a3
Laura Darlak <laura@polyporedesign.com>
2018-06-12 14:57:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -45,8 +45,7 @@ function addElementToBeginningOfArray(array, element) {
  }
  
  function removeElementFromEndOfArray(array){
-   array.slice(0, array.length - 1)
-   return array
+   return array.slice(0, array.length - 1)
  }
  
  
